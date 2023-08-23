8fc6741d030d6494549c96a85c965cddd79ffa01
Natalia Opazo Cuevas <natalia.opazo@ifop.cl>
2023-08-23 10:48:47 -0400

    Modelación completa

--- Ejemplo_clase3.R
+++ Ejemplo_clase3.R
@@ -85,3 +85,78 @@ cls_wf <- workflow() |>
   add_recipe(mod_recipe) |> 
   add_model(reg_log_cls)
 cls_wf
+
+## Parámetros
+# Nos sirve para buscar los parametros mejores para el modelo
+param_grid <- grid_regular(
+  penalty(), mixture(),
+  levels = c(10,10)
+)
+
+param_grid |> glimpse()
+
+## Ajuste ----
+start <- Sys.time()
+cls_wf_fit <- tune_grid(
+  cls_wf, climbers_fold,
+  grid = param_grid,
+  metrics = metric_set(roc_auc, accuracy, sens, spec), # métricas de desempeño
+  control = control_grid(save_pred = T, verbose = T) #Cómo lo voy a validar
+)
+Sys.time() - start
+
+## Desempeño ----
+cls_wf_fit |> collect_metrics(summarize = T)
+cls_wf_fit |>  show_best(metric = "roc_auc", n = 3)
+cls_wf_fit |>  select_best(metric = "roc_auc")
+
+cls_wf_fit %>% 
+  collect_predictions(
+    summarize = F, parameters = select_best(cls_wf_fit, metric = "roc_auc")
+  )
+## Gráfico de desempeño ----
+autoplot(cls_wf_fit)
+
+## Finalización del flujo ----
+cls_wf_final <- cls_wf %>% 
+  finalize_workflow(select_best(cls_wf_fit, metric = "roc_auc"))
+cls_wf_final
+
+## Predicción ----
+cls_wf_last_fit <- cls_wf_final %>% 
+  last_fit(split = climbers_split, metrics = metric_set(roc_auc, accuracy, sens, spec))
+cls_wf_last_fit
+
+# Entrega de resultados ----
+library(broom)
+library(tune)
+reg_log_cls_fit <- cls_wf_last_fit |> extract_fit_parsnip()
+
+## Componentes del modelo ----
+tidy(reg_log_cls_fit) |> glimpse()
+## Diagnóstico del modelo
+glance(reg_log_cls_fit) |>  glimpse()
+## Matriz de confusión ----
+collect_predictions(cls_wf_last_fit) |> 
+  conf_mat(died, estimate = .pred_class)
+## Sensibilidad ----
+collect_predictions(cls_wf_last_fit) |> sens(died, estimate = .pred_class)
+## Especificidad ----
+collect_predictions(cls_wf_last_fit) |>  
+  spec(died, estimate = .pred_class)
+## Precisión ----
+collect_predictions(cls_wf_last_fit) |> 
+  accuracy(died, estimate = .pred_class)
+## Todas ----
+metrics <- metric_set(accuracy, sens, spec)
+collect_predictions(cls_wf_last_fit) |>  
+  metrics(died, estimate = .pred_class)
+## Curvas ROC ----
+collect_predictions(cls_wf_fit) |> 
+  group_by(id) |> 
+  roc_curve(
+    died, .pred_TRUE,
+    event_level = "second"
+  ) |> 
+  autoplot()
+
